13ed39ab8a7dae14f438d406b5651fa17fc85863
mike <m.nagler995@gmail.com>
2026-04-12 19:08:12 -0400

    Format

--- asteroids/src/index.js
+++ asteroids/src/index.js
@@ -20,29 +20,29 @@ const labelYVelocity = document.getElementById("label--vy");
 const keyManager = new KeyManager();
 
 const ship = new Ship(
-    new Point(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2), 
-    0, 
-    ROCKET_HEIGHT, 
-    ROCKET_WIDTH, 
-    keyManager,
-    canvas,
-    ctx,
+  new Point(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2),
+  0,
+  ROCKET_HEIGHT,
+  ROCKET_WIDTH,
+  keyManager,
+  canvas,
+  ctx,
 );
 
 function updateLabels() {
-    labelAngle.innerHTML = `Angle: ${ship.rotation.toFixed(2)}`;
-    labelXVelocity.innerHTML = `V_x: ${ship.dX.toFixed(2)}`;
-    labelYVelocity.innerHTML = `V_y: ${ship.dY.toFixed(2)}`;
+  labelAngle.innerHTML = `Angle: ${ship.rotation.toFixed(2)}`;
+  labelXVelocity.innerHTML = `V_x: ${ship.dX.toFixed(2)}`;
+  labelYVelocity.innerHTML = `V_y: ${ship.dY.toFixed(2)}`;
 }
 
 function loop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ship.update();
-    ship.draw(ctx);
+  ship.update();
+  ship.draw(ctx);
 
-    updateLabels();
+  updateLabels();
 
-    requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 }
 loop();
--- asteroids/src/keyManager.js
+++ asteroids/src/keyManager.js
@@ -1,37 +1,37 @@
 class KeyManager {
-    constructor() {
+  constructor() {
+    this.cw = false;
+    this.ccw = false;
+    this.acc = false;
+    this.shoot = false;
+    this.setup();
+  }
+
+  setup() {
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "ArrowRight") {
+        this.cw = true;
+      }
+      if (event.key === "ArrowLeft") {
+        this.ccw = true;
+      }
+      if (event.key === "ArrowUp") {
+        this.acc = true;
+      }
+    });
+
+    document.addEventListener("keyup", (event) => {
+      if (event.key === "ArrowRight") {
         this.cw = false;
+      }
+      if (event.key === "ArrowLeft") {
         this.ccw = false;
+      }
+      if (event.key === "ArrowUp") {
         this.acc = false;
-        this.shoot = false;
-        this.setup();
-    } 
-
-    setup() {
-        document.addEventListener("keydown", (event) => {
-            if (event.key === "ArrowRight") {
-                this.cw = true;
-            }
-            if (event.key === "ArrowLeft") {
-                this.ccw = true;
-            }
-            if (event.key === "ArrowUp") {
-                this.acc = true;
-            }
-        });
-
-        document.addEventListener("keyup", (event) => {
-            if (event.key === "ArrowRight") {
-                this.cw = false;
-            }
-            if (event.key === "ArrowLeft") {
-                this.ccw = false;
-            }
-            if (event.key === "ArrowUp") {
-                this.acc = false;
-            }
-        });
-    }
+      }
+    });
+  }
 }
 
 export { KeyManager };
